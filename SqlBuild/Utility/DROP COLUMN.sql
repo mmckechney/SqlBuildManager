5c6ba891bb6133d9626aef524481e2c234c6b940
mmckechney <michael@mckechney.com>
2014-05-01 08:24:18 -0400

    Fixing issue with bad SQL in utility script

--- SqlBuild/Utility/DROP COLUMN.sql
+++ SqlBuild/Utility/DROP COLUMN.sql
@@ -1,5 +1,5 @@
 ${Title: Drop Column  <<Table Name>>.<<Column Name>>}
-IF EXISTS(SELECT 1 FROM s WITH (NOLOCK) WHERE TABLE_NAME = '<<Table Name>>' AND TABLE_SCHEMA = '<<Table Schema>>'AND COLUMN_NAME = '<<Column Name>>')
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.Columns WITH (NOLOCK) WHERE TABLE_NAME = '<<Table Name>>' AND TABLE_SCHEMA = '<<Table Schema>>'AND COLUMN_NAME = '<<Column Name>>')
 BEGIN
 	ALTER TABLE [<<Table Schema>>].[<<Table Name>>] DROP COLUMN <<Column Name>>
 END
