297585abc531b07f0229aac8eb248d362685ed63
mmckechney <michael@mckechney.com>
2014-07-16 16:29:29 -0400

    Getting started on database diff and sync

---
+++ SqlSync.SqlBuild/Syncronizer/DatabaseDiffer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using SqlSync.Connection;
+namespace SqlSync.SqlBuild.Syncronizer
+{
+    public class DatabaseDiffer
+    {
+
+        public void GetDatabaseRunHistory(ConnectionData dbConnData)
+        {
+            SqlConnection conn = SqlSync.Connection.ConnectionHelper.GetConnection(dbConnData);
+
+            string sql = @" SELECT BuildProjectHash, BuildFileName,  CommitDate, ScriptFileHash, ScriptId, ScriptFileName, Sequence 
+                    FROM SqlBuild_Logging ORDER BY commitDate DESC , sequence ASC";
+
+            DatabaseRunHistory history = new DatabaseRunHistory();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    bool filled;
+                    while (reader.Read())
+                    {
+                        var record =
+                            history.BuildFileHistory.FirstOrDefault(
+                                x => x.BuildFileHash == (string) reader["BuildProjectHash"]);
+                       if (record != null)
+                       {
+                           record.ScriptHistory.Add(new ScriptHistory()
+                               {
+                                   ScriptHash = (string) reader["ScriptFileHash"],
+                                   ScriptId = (string) reader["ScriptId"],
+                                   ScriptName = (string) reader["ScriptFileName"],
+                                   Sequence = (int) reader["Sequence"]
+                               });
+                       }
+                       else
+                       {
+                           BuildFileHistory h = new BuildFileHistory()
+                               {
+                                   BuildFileHash =(string)reader["BuildProjectHash"],
+                                   BuildFileName = (string)reader["BuildFileName"],
+                                   CommitDate = (DateTime)reader["CommitDate"]
+                               };
+                           h.ScriptHistory.Add(new ScriptHistory()
+                               {
+                                   ScriptHash = (string) reader["ScriptFileHash"],
+                                   ScriptId = (string) reader["ScriptId"],
+                                   ScriptName = (string) reader["ScriptFileName"],
+                                   Sequence = (int) reader["Sequence"]
+                               });
+                           history.BuildFileHistory.Add(h);
+                       }
+                    }
+                    conn.Close();
+                }
+            }
+            catch (SqlException)
+            {
+                //ignore
+            }
+            catch (Exception)
+            {
+                
+            }
+            finally
+            {
+                
+            }
+        }
+    }
+}
---
+++ SqlSync.SqlBuild/Syncronizer/DatabaseRunHistory.cs
@@ -0,0 +1,129 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+namespace SqlSync.SqlBuild.Syncronizer {
+
+
+    public partial class DatabaseRunHistory {
+        
+        private List<BuildFileHistory> buildFileHistoryField = new List<BuildFileHistory>();
+        
+
+        public List<BuildFileHistory> BuildFileHistory {
+            get {
+                return this.buildFileHistoryField;
+            }
+            set {
+                this.buildFileHistoryField = value;
+            }
+        }
+    }
+    
+       public partial class BuildFileHistory {
+        
+        private List<ScriptHistory> scriptHistoryField = new List<ScriptHistory>();
+        
+        private string buildFileNameField;
+        
+        private string buildFileHashField;
+        
+        private System.DateTime commitDateField;
+        
+        public BuildFileHistory() {
+            this.buildFileNameField = "";
+            this.buildFileHashField = "";
+            this.commitDateField = DateTime.MinValue;
+        }
+        
+        public List<ScriptHistory>  ScriptHistory {
+            get {
+                return this.scriptHistoryField;
+            }
+            set {
+                this.scriptHistoryField = value;
+            }
+        }
+        
+        public string BuildFileName {
+            get {
+                return this.buildFileNameField;
+            }
+            set {
+                this.buildFileNameField = value;
+            }
+        }
+        
+        public string BuildFileHash {
+            get {
+                return this.buildFileHashField;
+            }
+            set {
+                this.buildFileHashField = value;
+            }
+        }
+        
+        public System.DateTime CommitDate {
+            get {
+                return this.commitDateField;
+            }
+            set {
+                this.commitDateField = value;
+            }
+        }
+    }
+    
+
+    public partial class ScriptHistory {
+        
+        private string scriptNameField;
+        
+        private string scriptHashField;
+        
+        private int sequenceField;
+        
+        private string scriptIdField;
+        
+        public ScriptHistory() {
+            this.scriptNameField = "";
+            this.scriptHashField = "";
+            this.sequenceField = -1;
+            this.scriptIdField = "";
+        }
+        
+        public string ScriptName {
+            get {
+                return this.scriptNameField;
+            }
+            set {
+                this.scriptNameField = value;
+            }
+        }
+        
+        public string ScriptHash {
+            get {
+                return this.scriptHashField;
+            }
+            set {
+                this.scriptHashField = value;
+            }
+        }
+        
+        public int Sequence {
+            get {
+                return this.sequenceField;
+            }
+            set {
+                this.sequenceField = value;
+            }
+        }
+        
+        public string ScriptId {
+            get {
+                return this.scriptIdField;
+            }
+            set {
+                this.scriptIdField = value;
+            }
+        }
+    }
+}
