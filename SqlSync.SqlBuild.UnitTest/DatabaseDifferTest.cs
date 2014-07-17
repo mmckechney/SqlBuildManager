cc5882e63e9884ffd27d80c1888fd9ae1201a779
mmckechney <michael@mckechney.com>
2014-07-17 16:13:29 -0400

    WIP for database diffing - just getting it into the repo

---
+++ SqlSync.SqlBuild.UnitTest/DatabaseDifferTest.cs
@@ -0,0 +1,124 @@
+﻿using SqlSync.SqlBuild.Syncronizer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SqlSync.Connection;
+
+namespace SqlSync.SqlBuild.UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DatabaseDifferTest and is intended
+    ///to contain all DatabaseDifferTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DatabaseDifferTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for DatabaseDiffer Constructor
+        ///</summary>
+        [TestMethod()]
+        public void DatabaseDifferConstructorTest()
+        {
+            DatabaseDiffer target = new DatabaseDiffer();
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for GetDatabaseHistoryDifference
+        ///</summary>
+        [TestMethod()]
+        public void GetDatabaseHistoryDifferenceTest()
+        {
+            DatabaseDiffer target = new DatabaseDiffer(); 
+            ConnectionData goldenCopy = new ConnectionData()
+                {
+                    DatabaseName = "SqlBuildTest",
+                    SQLServerName = @"localhost\SQLEXPRESS",
+                    UseWindowAuthentication = true
+                };
+            ConnectionData toBeUpdated = new ConnectionData()
+                {
+                    DatabaseName = "SqlBuildTest1",
+                    SQLServerName = @"localhost\SQLEXPRESS",
+                    UseWindowAuthentication = true
+                };
+            DatabaseRunHistory expected = null; // TODO: Initialize to an appropriate value
+            DatabaseRunHistory actual;
+            actual = target.GetDatabaseHistoryDifference(goldenCopy, toBeUpdated);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for GetDatabaseRunHistory
+        ///</summary>
+        [TestMethod()]
+        public void GetDatabaseRunHistoryTest()
+        {
+            DatabaseDiffer target = new DatabaseDiffer(); // TODO: Initialize to an appropriate value
+            ConnectionData dbConnData = new ConnectionData()
+                {
+                    DatabaseName = "SqlBuildTest",
+                    SQLServerName = @"localhost\SQLEXPRESS",
+                    UseWindowAuthentication = true
+                };
+            DatabaseRunHistory expected = null; // TODO: Initialize to an appropriate value
+            DatabaseRunHistory actual;
+            actual = target.GetDatabaseRunHistory(dbConnData);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
--- SqlSync.SqlBuild/Syncronizer/DatabaseDiffer.cs
+++ SqlSync.SqlBuild/Syncronizer/DatabaseDiffer.cs
@@ -5,17 +5,34 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using SqlSync.Connection;
+using log4net;
 namespace SqlSync.SqlBuild.Syncronizer
 {
     public class DatabaseDiffer
     {
+        private static ILog log = log4net.LogManager.GetLogger(typeof(DatabaseDiffer));
 
-        public void GetDatabaseRunHistory(ConnectionData dbConnData)
+        public DatabaseRunHistory GetDatabaseHistoryDifference(ConnectionData goldenCopy, ConnectionData toBeUpdated)
+        {
+            Stack<BuildFileHistory> q = new Stack<BuildFileHistory>();
+
+            var golden = GetDatabaseRunHistory(goldenCopy).BuildFileHistory.OrderByDescending(x => x.CommitDate);
+            var toUpdate = GetDatabaseRunHistory(toBeUpdated).BuildFileHistory.OrderByDescending(x => x.CommitDate);
+
+            var unique = golden.Where(p => !toUpdate.Any(p2 => p2.BuildFileHash == p.BuildFileHash));
+           
+          
+            return null;
+        }
+
+        public DatabaseRunHistory GetDatabaseRunHistory(ConnectionData dbConnData)
         {
             SqlConnection conn = SqlSync.Connection.ConnectionHelper.GetConnection(dbConnData);
 
             string sql = @" SELECT BuildProjectHash, BuildFileName,  CommitDate, ScriptFileHash, ScriptId, ScriptFileName, Sequence 
-                    FROM SqlBuild_Logging ORDER BY commitDate DESC , sequence ASC";
+                    FROM SqlBuild_Logging 
+                    WHERE BuildProjectHash <> '' AND BuildProjectHash IS NOT NULL
+                    ORDER BY commitDate DESC , sequence ASC";
 
             DatabaseRunHistory history = new DatabaseRunHistory();
 
@@ -28,52 +45,51 @@ namespace SqlSync.SqlBuild.Syncronizer
                     bool filled;
                     while (reader.Read())
                     {
-                        var record =
-                            history.BuildFileHistory.FirstOrDefault(
-                                x => x.BuildFileHash == (string) reader["BuildProjectHash"]);
-                       if (record != null)
+                        //TODO: Change this to a loop since we know they are in order?
+                        var record = history.BuildFileHistory.FirstOrDefault(x => x.BuildFileHash == (string) reader["BuildProjectHash"]);
+
+                        if (record == null)
+                        {
+                            BuildFileHistory h = new BuildFileHistory()
+                               {
+                                   BuildFileHash = reader["BuildProjectHash"].ToString(),
+                                   BuildFileName =  reader["BuildFileName"].ToString(),
+                                   CommitDate = (DateTime)reader["CommitDate"]
+                               };
+                           h.ScriptHistory.Add(new ScriptHistory()
+                               {
+                                   ScriptHash = reader["ScriptFileHash"].ToString(),
+                                   ScriptId = reader["ScriptId"].ToString(),
+                                   ScriptName = reader["ScriptFileName"].ToString(),
+                                   Sequence = (int) reader["Sequence"]
+                               });
+                           history.BuildFileHistory.Add(h);
+                        }
+                     
+                        else if (record.CommitDate == (DateTime) reader["CommitDate"])
                        {
                            record.ScriptHistory.Add(new ScriptHistory()
                                {
-                                   ScriptHash = (string) reader["ScriptFileHash"],
-                                   ScriptId = (string) reader["ScriptId"],
-                                   ScriptName = (string) reader["ScriptFileName"],
+                                   ScriptHash = reader["ScriptFileHash"].ToString(),
+                                   ScriptId = reader["ScriptId"].ToString(),
+                                   ScriptName = reader["ScriptFileName"].ToString(),
                                    Sequence = (int) reader["Sequence"]
                                });
                        }
                        else
                        {
-                           BuildFileHistory h = new BuildFileHistory()
-                               {
-                                   BuildFileHash =(string)reader["BuildProjectHash"],
-                                   BuildFileName = (string)reader["BuildFileName"],
-                                   CommitDate = (DateTime)reader["CommitDate"]
-                               };
-                           h.ScriptHistory.Add(new ScriptHistory()
-                               {
-                                   ScriptHash = (string) reader["ScriptFileHash"],
-                                   ScriptId = (string) reader["ScriptId"],
-                                   ScriptName = (string) reader["ScriptFileName"],
-                                   Sequence = (int) reader["Sequence"]
-                               });
-                           history.BuildFileHistory.Add(h);
+                           //this is an older record for the same build file, so ignore it...
                        }
                     }
                     conn.Close();
+
                 }
             }
-            catch (SqlException)
+            catch (Exception exe)
             {
-                //ignore
+                log.Error(String.Format("Unable to get build history for {0}.{1}", dbConnData.SQLServerName, dbConnData.DatabaseName),exe);
             }
-            catch (Exception)
-            {
-                
-            }
-            finally
-            {
-                
-            }
+            return history;
         }
     }
 }
