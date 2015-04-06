d83690d1fc616e3d7000a0f4ebdc8bee086fca92
Michael McKechney <michael@mckechney.com>
2015-04-06 17:08:51 -0400

    Updating VB project version

--- SqlSync.ScrollBarSync/My Project/AssemblyInfo.vb
+++ SqlSync.ScrollBarSync/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("Michael McKechney - www.mckechney.com")> 
 <Assembly: AssemblyProduct("SqlSync.ScrollBarSync")> 
-<Assembly: AssemblyCopyright("Copyright © Michael McKechney 2004-2011")> 
+<Assembly: AssemblyCopyright("Copyright © Michael McKechney 2004-2015")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("9.2.2")> 
-<Assembly: AssemblyFileVersion("9.2.2")> 
+<Assembly: AssemblyVersion("9.3.0")> 
+<Assembly: AssemblyFileVersion("9.3.0")> 
