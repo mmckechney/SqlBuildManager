0f1a9f20952176f2ec4e3cb36330e4458902c224
Michael McKechney <michael@mckechney.com>
2018-06-12 18:28:52 +0000

    Added batch Nuget

---
+++ SqlBuildManager.Console/BatchExecution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Batch.FileStaging;
+using Microsoft.Azure.Batch.Common;
+namespace SqlBuildManager.Console
+{
+    public class BatchExecution
+    {
+        
+    }
+}
