7dcc217df65cc787d549fb4f66c51ce0f6a3f170
Michael McKechney <michael@mckechney.com>
2018-06-27 21:44:13 +0000

    Solved problem with Compression extension methods missing. Decompiled and copied them in myself! [Method not found: 'System.IO.Compression.ZipArchive System.IO.Compression.ZipFile.Open]

--- SqlSync.SqlBuild/ZipHelper.cs
+++ SqlSync.SqlBuild/ZipHelper.cs
@@ -10,23 +10,23 @@ namespace SqlSync.SqlBuild
     /// Summary description for ZipHelper.
     /// </summary>
     public class ZipHelper
-	{
+    {
         private static ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-		public ZipHelper()
-		{
+        public ZipHelper()
+        {
 
         }
 
-        public static bool CreateZipPackage(string[] filesToZip, string basePath, string zipFileName,bool keepPathInfo)
-		{
+        public static bool CreateZipPackage(string[] filesToZip, string basePath, string zipFileName, bool keepPathInfo)
+        {
             return CreateZipPackage(filesToZip, basePath, zipFileName, keepPathInfo, 0);
-		}
-		public static bool CreateZipPackage(string[] filesToZip, string basePath, string zipFileName)
-		{
-			return CreateZipPackage(filesToZip,basePath,zipFileName,true);
-		}
-       
+        }
+        public static bool CreateZipPackage(string[] filesToZip, string basePath, string zipFileName)
+        {
+            return CreateZipPackage(filesToZip, basePath, zipFileName, true);
+        }
+
         public static bool CreateZipPackage(List<string> fullPathFilesToZip, string zipFileName, bool keepPathInfo, int retryCount)
         {
 
@@ -40,7 +40,7 @@ namespace SqlSync.SqlBuild
                         if (File.Exists(file) == false)
                             continue;
 
-                        newFile.CreateEntryFromFile(file, Path.GetFileName(file));
+                        newFile.CreateEntryFromFile(file, Path.GetFileName(file),CompressionLevel.Fastest);
 
                     }
                 }
@@ -65,7 +65,7 @@ namespace SqlSync.SqlBuild
             }
             return true;
         }
-            
+
         private static bool CreateZipPackage(string[] filesToZip, string basePath, string zipFileName, bool keepPathInfo, int retryCount)
         {
             if (basePath.Trim().Length > 0 && !basePath.EndsWith(@"\"))
@@ -76,7 +76,7 @@ namespace SqlSync.SqlBuild
             {
                 fullPathFiles.Add(basePath + file);
             }
-            return  CreateZipPackage(fullPathFiles, zipFileName, keepPathInfo, retryCount);
+            return CreateZipPackage(fullPathFiles, zipFileName, keepPathInfo, retryCount);
         }
 
         public static bool UnpackZipPackage(string destinationDir, string zipFileName)
@@ -89,7 +89,7 @@ namespace SqlSync.SqlBuild
                 {
                     Directory.CreateDirectory(destinationDir);
                 }
-                using (ZipArchive archive = ZipFile.Open(zipFileName,ZipArchiveMode.Read))
+                using (ZipArchive archive = ZipFile.Open(zipFileName, ZipArchiveMode.Read))
                 {
                     //Loops through each file in the zip file
                     foreach (ZipArchiveEntry file in archive.Entries)
@@ -121,7 +121,7 @@ namespace SqlSync.SqlBuild
                     foreach (var file in filesToZip)
                     {
                         log.DebugFormat("Adding files '{0}' to package zip file '{1}'", file, filesToZip);
-                        modFile.CreateEntryFromFile(file, Path.GetFileName(file));
+                        modFile.CreateEntryFromFile(file, Path.GetFileName(file),CompressionLevel.Fastest);
                     }
 
                 }
@@ -133,7 +133,99 @@ namespace SqlSync.SqlBuild
                 return false;
             }
 
-           
+
+        }
+
+
+
+    }
+    public static class ZipFile
+    {
+        public static ZipArchive Open(string archiveFileName, ZipArchiveMode mode)
+        {
+            FileMode mode1;
+            FileAccess access;
+            FileShare share;
+            switch (mode)
+            {
+                case ZipArchiveMode.Read:
+                    mode1 = FileMode.Open;
+                    access = FileAccess.Read;
+                    share = FileShare.Read;
+                    break;
+                case ZipArchiveMode.Create:
+                    mode1 = FileMode.CreateNew;
+                    access = FileAccess.Write;
+                    share = FileShare.None;
+                    break;
+                case ZipArchiveMode.Update:
+                    mode1 = FileMode.OpenOrCreate;
+                    access = FileAccess.ReadWrite;
+                    share = FileShare.None;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mode));
+            }
+            FileStream fileStream = (FileStream)null;
+            try
+            {
+                fileStream = File.Open(archiveFileName, mode1, access, share);
+                return new ZipArchive((Stream)fileStream, mode, false, null);
+            }
+            catch
+            {
+                fileStream?.Dispose();
+                throw;
+            }
+        }
+    }
+
+    public static class ZipFileExtensions
+    {
+        public static ZipArchiveEntry CreateEntryFromFile(this ZipArchive destination, string sourceFileName, string entryName, CompressionLevel compressionLevel)
+        {
+            return ZipFileExtensions.DoCreateEntryFromFile(destination, sourceFileName, entryName, new CompressionLevel?(compressionLevel));
+        }
+        internal static ZipArchiveEntry DoCreateEntryFromFile(ZipArchive destination, string sourceFileName, string entryName, CompressionLevel? compressionLevel)
+        {
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
+            if (sourceFileName == null)
+                throw new ArgumentNullException(nameof(sourceFileName));
+            if (entryName == null)
+                throw new ArgumentNullException(nameof(entryName));
+            using (Stream stream = (Stream)File.Open(sourceFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                ZipArchiveEntry zipArchiveEntry = compressionLevel.HasValue ? destination.CreateEntry(entryName, compressionLevel.Value) : destination.CreateEntry(entryName);
+                DateTime dateTime = File.GetLastWriteTime(sourceFileName);
+                if (dateTime.Year < 1980 || dateTime.Year > 2107)
+                    dateTime = new DateTime(1980, 1, 1, 0, 0, 0);
+                zipArchiveEntry.LastWriteTime = (DateTimeOffset)dateTime;
+                using (Stream destination1 = zipArchiveEntry.Open())
+                    stream.CopyTo(destination1);
+                return zipArchiveEntry;
+            }
+        }
+
+        public static void ExtractToFile(this ZipArchiveEntry source, string destinationFileName)
+        {
+            source.ExtractToFile(destinationFileName, false);
+        }
+
+        public static void ExtractToFile(this ZipArchiveEntry source, string destinationFileName, bool overwrite)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (destinationFileName == null)
+                throw new ArgumentNullException(nameof(destinationFileName));
+            FileMode mode = overwrite ? FileMode.Create : FileMode.CreateNew;
+            using (Stream destination = (Stream)File.Open(destinationFileName, mode, FileAccess.Write, FileShare.None))
+            {
+                using (Stream stream = source.Open())
+                    stream.CopyTo(destination);
+            }
+            File.SetLastWriteTime(destinationFileName, source.LastWriteTime.DateTime);
         }
     }
 }
+
