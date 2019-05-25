f7e9a8ef23e286d72640ecfb84c133d72c2bb2d0
Michael McKechney <mimcke@microsoft.com>
2019-05-25 18:43:45 -0400

    reorganizing source

--- SqlBuildManager.Services.Azure/SqlBuildManager.ServicesContent/bin/install-ad.cmd
+++
@@ -1,9 +0,0 @@
-
-set adalsqlinstallfile=adalsql.msi
-
-"%~dp0%adalsqlinstallfile%" /q  /log c:\startuplog.log
-
-:end
-echo install-ad.cmd completed: %date:~-4,4%%date:~-10,2%%date:~-7,2%-%timehour: =0%%time:~3,2%
-
-EXIT /B 0
