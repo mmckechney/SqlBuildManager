7f81187d9a5f9e39e9991d33ad59a2da3676666a
Michael McKechney <mimcke@microsoft.com>
2020-01-02 18:50:23 -0500

    Updates test packages Starting to separate database dependent unit tests from true unit tests

---
+++ src/SqlSync.SqlBuild.Dependent.UnitTest/Resources/CreateTestTablesScript.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'TransactionTest' AND TABLE_SCHEMA = 'dbo')
+BEGIN
+	CREATE TABLE [dbo].[TransactionTest](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Message] [varchar](200) NULL,
+	[Guid] [uniqueidentifier] NULL,
+	[DateTimeStamp] [datetime] NULL
+) ON [PRIMARY]
+END
--- src/SqlSync.SqlBuild.UnitTest/Properties/AssemblyInfo.cs
+++ src/SqlSync.SqlBuild.UnitTest/Properties/AssemblyInfo.cs
@@ -1,22 +1,20 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
 [assembly: AssemblyTitle("SqlSync.SqlBuild.UnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SqlSync.SqlBuild.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-//[assembly: EnvironmentPermissionAttribute(SecurityAction.RequestMinimum)]
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52430f01-4604-4055-bcfa-7074ed65a454")]
+[assembly: Guid("bb28836a-aa83-49f0-802f-39d6847e3949")]
 
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
