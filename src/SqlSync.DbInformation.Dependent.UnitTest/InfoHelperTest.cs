c4691dea2c86bb3751731292c853e1bfb38eb655
Michael McKechney <michael@mckechney.com>
2026-01-30 12:09:42 -0500

    Adding local database integration tests

--- src/SqlSync.DbInformation.Dependent.UnitTest/InfoHelperTest.cs
+++ src/SqlSync.DbInformation.Dependent.UnitTest/InfoHelperTest.cs
@@ -1,83 +1,415 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlSync.Connection;
+using System.Data;
+using System.Linq;
 
 namespace SqlSync.DbInformation.Dependent.UnitTest
 {
-
-
     /// <summary>
-    ///This is a test class for InfoHelperTest and is intended
-    ///to contain all InfoHelperTest Unit Tests
-    ///</summary>
+    /// Integration tests for InfoHelper that require database access
+    /// NOTE: These tests require (local)\SQLEXPRESS with SqlBuildTest databases
+    /// </summary>
     [TestClass()]
     public class InfoHelperTest
     {
+        private static Initialization? _init;
 
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
+        [ClassInitialize()]
+        public static void ClassInitialize(TestContext testContext)
         {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
+            _init = new Initialization();
         }
 
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
+        private ConnectionData GetConnData() => _init?.connData ?? new ConnectionData(@"(local)\SQLEXPRESS", "SqlBuildTest");
 
+        #region GetColumnNames Tests
 
-        /// <summary>
-        ///A test for GetColumnNames
-        ///</summary>
         [TestMethod()]
-        public void GetColumnNamesTest()
+        public void GetColumnNames_SqlBuildLogging_ReturnsExpectedColumns()
         {
-            Initialization init = new Initialization();
             string tableName = "SqlBuild_Logging";
-            ConnectionData connData = init.connData;
-            string[] actual;
-            actual = InfoHelper.GetColumnNames(tableName, connData);
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetColumnNames(tableName, connData);
+
             Assert.AreEqual(18, actual.Length);
             Assert.AreEqual("BuildFileName", actual[0]);
             Assert.AreEqual("TargetDatabase", actual[11]);
             Assert.AreEqual("BuildRequestedBy", actual[14]);
         }
+
+        [TestMethod()]
+        public void GetColumnNames_TransactionTest_ReturnsExpectedColumns()
+        {
+            string tableName = "TransactionTest";
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetColumnNames(tableName, connData);
+
+            Assert.IsTrue(actual.Length >= 3);
+            Assert.IsTrue(actual.Contains("Message") || actual.Any(c => c.ToLower().Contains("message")));
+        }
+
+        [TestMethod()]
+        public void GetColumnNames_WithSchemaPrefix_ReturnsColumns()
+        {
+            string tableName = "dbo.SqlBuild_Logging";
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetColumnNames(tableName, connData);
+
+            Assert.IsTrue(actual.Length > 0, "Should return columns when schema is specified");
+        }
+
+        #endregion
+
+        #region GetColumnNamesWithTypes Tests
+
+        [TestMethod()]
+        public void GetColumnNamesWithTypes_SqlBuildLogging_ReturnsColumnInfo()
+        {
+            string tableName = "SqlBuild_Logging";
+            ConnectionData connData = GetConnData();
+
+            ColumnInfo[] actual = InfoHelper.GetColumnNamesWithTypes(tableName, connData);
+
+            Assert.IsTrue(actual.Length > 0, "Should return column info");
+            Assert.IsTrue(actual.Any(c => c.ColumnName == "BuildFileName"));
+        }
+
+        [TestMethod()]
+        public void GetColumnNamesWithTypes_ReturnsDataTypes()
+        {
+            string tableName = "SqlBuild_Logging";
+            ConnectionData connData = GetConnData();
+
+            ColumnInfo[] actual = InfoHelper.GetColumnNamesWithTypes(tableName, connData);
+
+            Assert.IsTrue(actual.All(c => !string.IsNullOrEmpty(c.DataType)), "All columns should have data types");
+        }
+
+        #endregion
+
+        #region GetPrimaryKeyColumns Tests
+
+        [TestMethod()]
+        public void GetPrimaryKeyColumns_TableWithPK_ReturnsPKColumns()
+        {
+            // SqlBuild_Logging may not have a PK, so we test for empty result handling
+            string tableName = "SqlBuild_Logging";
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetPrimaryKeyColumns(tableName, connData);
+
+            // Result can be empty if table has no PK - just verify it doesn't throw
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod()]
+        public void GetPrimaryKeyColumns_WithSchema_Works()
+        {
+            string tableName = "dbo.SqlBuild_Logging";
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetPrimaryKeyColumns(tableName, connData);
+
+            Assert.IsNotNull(actual);
+        }
+
+        #endregion
+
+        #region GetDatabaseTableList Tests
+
+        [TestMethod()]
+        public void GetDatabaseTableList_ReturnsTablesInDatabase()
+        {
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetDatabaseTableList(connData);
+
+            Assert.IsTrue(actual.Length > 0, "Should return at least one table");
+            Assert.IsTrue(actual.Any(t => t.Contains("SqlBuild_Logging")), "Should contain SqlBuild_Logging table");
+        }
+
+        [TestMethod()]
+        public void GetDatabaseTableList_WithFilter_FiltersResults()
+        {
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetDatabaseTableList(connData, "SqlBuild%");
+
+            Assert.IsTrue(actual.Length > 0, "Should return at least one table matching filter");
+            Assert.IsTrue(actual.All(t => t.Contains("SqlBuild")), "All tables should match filter");
+        }
+
+        [TestMethod()]
+        public void GetDatabaseTableList_NoMatchingFilter_ReturnsEmpty()
+        {
+            ConnectionData connData = GetConnData();
+
+            string[] actual = InfoHelper.GetDatabaseTableList(connData, "ZZZZNONEXISTENT%");
+
+            Assert.AreEqual(0, actual.Length, "Should return empty array for non-matching filter");
+        }
+
+        #endregion
+
+        #region GetDatabaseTableListWithRowCount Tests
+
+        [TestMethod()]
+        public void GetDatabaseTableListWithRowCount_ReturnsTables()
+        {
+            ConnectionData connData = GetConnData();
+
+            TableSize[] actual = InfoHelper.GetDatabaseTableListWithRowCount(connData);
+
+            Assert.IsTrue(actual.Length > 0, "Should return at least one table");
+        }
+
+        [TestMethod()]
+        public void GetDatabaseTableListWithRowCount_IncludesRowCounts()
+        {
+            ConnectionData connData = GetConnData();
+
+            TableSize[] actual = InfoHelper.GetDatabaseTableListWithRowCount(connData);
+
+            Assert.IsTrue(actual.All(t => t.RowCount >= -1), "Row counts should be valid");
+        }
+
+        [TestMethod()]
+        public void GetDatabaseTableListWithRowCount_WithFilter_FiltersResults()
+        {
+            ConnectionData connData = GetConnData();
+
+            TableSize[] actual = InfoHelper.GetDatabaseTableListWithRowCount(connData, "SqlBuild%");
+
+            Assert.IsTrue(actual.All(t => t.TableName.Contains("SqlBuild")));
+        }
+
+        #endregion
+
+        #region DbContainsTable Tests
+
+        [TestMethod()]
+        public void DbContainsTable_ExistingTable_ReturnsTrue()
+        {
+            ConnectionData connData = GetConnData();
+
+            bool actual = InfoHelper.DbContainsTable("dbo.SqlBuild_Logging", connData);
+
+            Assert.IsTrue(actual, "Should find existing table");
+        }
+
+        [TestMethod()]
+        public void DbContainsTable_NonExistingTable_ReturnsFalse()
+        {
+            ConnectionData connData = GetConnData();
+
+            bool actual = InfoHelper.DbContainsTable("dbo.NonExistentTable12345", connData);
+
+            Assert.IsFalse(actual, "Should not find non-existent table");
+        }
+
+        [TestMethod()]
+        public void DbContainsTable_CaseInsensitive_Works()
+        {
+            ConnectionData connData = GetConnData();
+
+            bool actual = InfoHelper.DbContainsTable("DBO.SQLBUILD_LOGGING", connData);
+
+            Assert.IsTrue(actual, "Should find table case-insensitively");
+        }
+
+        [TestMethod()]
+        public void DbContainsTable_FromArray_Works()
+        {
+            string[] tables = new[] { "dbo.Table1", "dbo.SqlBuild_Logging", "dbo.Table3" };
+
+            bool actual = InfoHelper.DbContainsTable("dbo.SqlBuild_Logging", tables);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void DbContainsTable_FromArray_NotFound_ReturnsFalse()
+        {
+            string[] tables = new[] { "dbo.Table1", "dbo.Table2", "dbo.Table3" };
+
+            bool actual = InfoHelper.DbContainsTable("dbo.NonExistent", tables);
+
+            Assert.IsFalse(actual);
+        }
+
+        #endregion
+
+        #region DbContainsTableWithRowcount Tests
+
+        [TestMethod()]
+        public void DbContainsTableWithRowcount_ExistingTable_ReturnsRowCount()
+        {
+            TableSize[] tableData = new[]
+            {
+                new TableSize { TableName = "dbo.Table1", RowCount = 100 },
+                new TableSize { TableName = "dbo.SqlBuild_Logging", RowCount = 50 }
+            };
+
+            int actual = InfoHelper.DbContainsTableWithRowcount("dbo.SqlBuild_Logging", tableData);
+
+            Assert.AreEqual(50, actual);
+        }
+
+        [TestMethod()]
+        public void DbContainsTableWithRowcount_NonExistingTable_ReturnsNegative()
+        {
+            TableSize[] tableData = new[]
+            {
+                new TableSize { TableName = "dbo.Table1", RowCount = 100 }
+            };
+
+            int actual = InfoHelper.DbContainsTableWithRowcount("dbo.NonExistent", tableData);
+
+            Assert.AreEqual(-1, actual);
+        }
+
+        #endregion
+
+        #region GetDatabaseList Tests
+
+        [TestMethod()]
+        public void GetDatabaseList_ReturnsMultipleDatabases()
+        {
+            ConnectionData connData = GetConnData();
+
+            DatabaseList actual = InfoHelper.GetDatabaseList(connData);
+
+            Assert.IsTrue(actual.Count > 0, "Should return at least one database");
+        }
+
+        [TestMethod()]
+        public void GetDatabaseList_ContainsSqlBuildTest()
+        {
+            ConnectionData connData = GetConnData();
+
+            DatabaseList actual = InfoHelper.GetDatabaseList(connData);
+
+            Assert.IsTrue(actual.Any(d => d.DatabaseName.StartsWith("SqlBuildTest")),
+                "Should contain SqlBuildTest database");
+        }
+
+        [TestMethod()]
+        public void GetDatabaseList_WithErrorFlag_ReportsSuccess()
+        {
+            ConnectionData connData = GetConnData();
+
+            DatabaseList actual = InfoHelper.GetDatabaseList(connData, out bool hasError);
+
+            Assert.IsFalse(hasError, "Should not have error on valid connection");
+            Assert.IsTrue(actual.Count > 0);
+        }
+
+        #endregion
+
+        #region GetTableObjectList Tests
+
+        [TestMethod()]
+        public void GetTableObjectList_ReturnsTableObjects()
+        {
+            ConnectionData connData = GetConnData();
+
+            var actual = InfoHelper.GetTableObjectList(connData);
+
+            Assert.IsTrue(actual.Count > 0, "Should return at least one table object");
+        }
+
+        [TestMethod()]
+        public void GetTableObjectList_ObjectsHaveTableType()
+        {
+            ConnectionData connData = GetConnData();
+
+            var actual = InfoHelper.GetTableObjectList(connData);
+
+            Assert.IsTrue(actual.All(o => o.ObjectType == "Table"), "All objects should be Tables");
+        }
+
+        #endregion
+
+        #region GetColumnNames with DataTable Tests
+
+        [TestMethod()]
+        public void GetColumnNames_FromDataTable_ReturnsColumnNames()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Column1");
+            table.Columns.Add("Column2");
+            table.Columns.Add("Column3");
+
+            string[] actual = InfoHelper.GetColumnNames(table);
+
+            Assert.AreEqual(3, actual.Length);
+            Assert.AreEqual("Column1", actual[0]);
+            Assert.AreEqual("Column2", actual[1]);
+            Assert.AreEqual("Column3", actual[2]);
+        }
+
+        [TestMethod()]
+        public void GetColumnNames_FromEmptyDataTable_ReturnsEmptyArray()
+        {
+            DataTable table = new DataTable();
+
+            string[] actual = InfoHelper.GetColumnNames(table);
+
+            Assert.AreEqual(0, actual.Length);
+        }
+
+        #endregion
+
+        #region ExtractNameAndSchema Tests
+
+        [TestMethod()]
+        public void ExtractNameAndSchema_WithSchema_ExtractsBoth()
+        {
+            InfoHelper.ExtractNameAndSchema("dbo.MyTable", out string tableName, out string schema);
+
+            Assert.AreEqual("MyTable", tableName);
+            Assert.AreEqual("dbo", schema);
+        }
+
+        [TestMethod()]
+        public void ExtractNameAndSchema_WithoutSchema_DefaultsToDbo()
+        {
+            InfoHelper.ExtractNameAndSchema("MyTable", out string tableName, out string schema);
+
+            Assert.AreEqual("MyTable", tableName);
+            Assert.AreEqual("dbo", schema);
+        }
+
+        [TestMethod()]
+        public void ExtractNameAndSchema_WithBrackets_HandlesBrackets()
+        {
+            InfoHelper.ExtractNameAndSchema("[dbo].[MyTable]", out string tableName, out string schema);
+
+            // Should handle brackets properly
+            Assert.IsNotNull(tableName);
+            Assert.IsNotNull(schema);
+        }
+
+        #endregion
+
+        #region GetDatabaseSizeAnalysis Tests
+
+        [TestMethod()]
+        [Ignore("Resource file SizeAnalysis.sql is not properly embedded - TODO: fix resource embedding")]
+        public void GetDatabaseSizeAnalysis_ReturnsAnalysisData()
+        {
+            ConnectionData connData = GetConnData();
+
+            var actual = InfoHelper.GetDatabaseSizeAnalysis(connData);
+
+            Assert.IsNotNull(actual);
+            // Analysis may return empty list if database is empty
+            Assert.IsNotNull(actual.SizeAnalysis);
+        }
+
+        #endregion
     }
 }
--- src/SqlSync.SqlBuild.Dependent.UnitTest/DatabaseDifferTest.cs
+++ src/SqlSync.SqlBuild.Dependent.UnitTest/DatabaseDifferTest.cs
@@ -2,64 +2,51 @@
 using SqlSync.Connection;
 using SqlSync.SqlBuild.Synchronizer;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 #nullable enable
 
 namespace SqlSync.SqlBuild.Dependent.UnitTest
 {
-
-
     /// <summary>
-    ///This is a test class for DatabaseDifferTest and is intended
-    ///to contain all DatabaseDifferTest Unit Tests
-    ///</summary>
+    /// Integration tests for DatabaseDiffer that require database access
+    /// NOTE: These tests require (local)\SQLEXPRESS with SqlBuildTest databases
+    /// </summary>
     [TestClass()]
+    [DoNotParallelize]
     public class DatabaseDifferTest
     {
+        private static List<Initialization> _initColl = new();
 
         [ClassInitialize()]
         public static void Initilize(TestContext testContext)
         {
-            Initialization init = new Initialization();
+            _initColl = new List<Initialization>();
+            // Create one to ensure databases exist
+            _initColl.Add(new Initialization());
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            foreach (var init in _initColl)
+            {
+                init.Dispose();
+            }
+        }
+
+        private Initialization GetInitialization()
+        {
+            var init = new Initialization();
+            _initColl.Add(init);
+            return init;
         }
 
         public TestContext TestContext { get; set; } = null!;
 
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
+        #region Constructor Tests
 
-
-        /// <summary>
-        ///A test for DatabaseDiffer Constructor
-        ///</summary>
         [TestMethod()]
         public void DatabaseDifferConstructorTest()
         {
@@ -67,9 +54,143 @@ namespace SqlSync.SqlBuild.Dependent.UnitTest
             Assert.IsNotNull(target);
         }
 
-        /// <summary>
-        ///A test for GetDatabaseHistoryDifference
-        ///</summary>
+        #endregion
+
+        #region GetDatabaseRunHistory Tests
+
+        [TestMethod()]
+        public void GetDatabaseRunHistory_ReturnsHistory()
+        {
+            var init = GetInitialization();
+            DatabaseDiffer target = new DatabaseDiffer();
+            ConnectionData connData = new ConnectionData()
+            {
+                DatabaseName = init.testDatabaseNames[0],
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+
+            DatabaseRunHistory actual = target.GetDatabaseRunHistory(connData);
+
+            Assert.IsNotNull(actual);
+            Assert.IsNotNull(actual.BuildFileHistory);
+            // May be empty if no builds have been run
+        }
+
+        [TestMethod()]
+        public void GetDatabaseRunHistory_MultipleDbsReturnDifferentHistory()
+        {
+            var init = GetInitialization();
+            DatabaseDiffer target = new DatabaseDiffer();
+            
+            ConnectionData connData1 = new ConnectionData()
+            {
+                DatabaseName = init.testDatabaseNames[0],
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+            
+            ConnectionData connData2 = new ConnectionData()
+            {
+                DatabaseName = init.testDatabaseNames.Count > 1 ? init.testDatabaseNames[1] : init.testDatabaseNames[0],
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+
+            DatabaseRunHistory history1 = target.GetDatabaseRunHistory(connData1);
+            DatabaseRunHistory history2 = target.GetDatabaseRunHistory(connData2);
+
+            Assert.IsNotNull(history1);
+            Assert.IsNotNull(history2);
+        }
+
+        [TestMethod()]
+        public void GetDatabaseRunHistory_HandlesEmptyLoggingTable()
+        {
+            var init = GetInitialization();
+            // Clean test databases should have empty logging tables
+            DatabaseDiffer target = new DatabaseDiffer();
+            ConnectionData connData = new ConnectionData()
+            {
+                DatabaseName = init.testDatabaseNames[0],
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+
+            DatabaseRunHistory actual = target.GetDatabaseRunHistory(connData);
+
+            Assert.IsNotNull(actual);
+            // Empty is valid - the table exists but has no data
+        }
+
+        #endregion
+
+        #region GetDatabaseHistoryDifference Tests
+
+        [TestMethod()]
+        public void GetDatabaseHistoryDifferenceTest_SameSource()
+        {
+            var init = GetInitialization();
+            DatabaseDiffer target = new DatabaseDiffer();
+            ConnectionData goldenCopy = new ConnectionData()
+            {
+                DatabaseName = init.testDatabaseNames[0],
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+            ConnectionData toBeUpdated = new ConnectionData()
+            {
+                DatabaseName = init.testDatabaseNames[0],
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+
+            DatabaseRunHistory actual = target.GetDatabaseHistoryDifference(goldenCopy, toBeUpdated);
+
+            Assert.AreEqual(0, actual.BuildFileHistory.Count, "Same database should have no differences");
+        }
+
+        [TestMethod()]
+        public void GetDatabaseHistoryDifference_StringOverload_Works()
+        {
+            var init = GetInitialization();
+            DatabaseDiffer target = new DatabaseDiffer();
+
+            DatabaseRunHistory actual = target.GetDatabaseHistoryDifference(
+                init.serverName, init.testDatabaseNames[0],
+                init.serverName, init.testDatabaseNames[0]);
+
+            Assert.AreEqual(0, actual.BuildFileHistory.Count);
+        }
+
+        [TestMethod()]
+        public void GetDatabaseHistoryDifference_DifferentDatabases_ReturnsDifferences()
+        {
+            var init = GetInitialization();
+            DatabaseDiffer target = new DatabaseDiffer();
+            ConnectionData goldenCopy = new ConnectionData()
+            {
+                DatabaseName = init.testDatabaseNames[0],
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+            
+            // Use a different database if available
+            string otherDb = init.testDatabaseNames.Count > 1 ? init.testDatabaseNames[1] : init.testDatabaseNames[0];
+            ConnectionData toBeUpdated = new ConnectionData()
+            {
+                DatabaseName = otherDb,
+                SQLServerName = init.serverName,
+                AuthenticationType = AuthenticationType.Windows
+            };
+
+            DatabaseRunHistory actual = target.GetDatabaseHistoryDifference(goldenCopy, toBeUpdated);
+
+            Assert.IsNotNull(actual);
+            Assert.IsNotNull(actual.BuildFileHistory);
+            // Differences depend on what's been run on each database
+        }
+
         [TestMethod(), Ignore("Don't have the setup scripts ready yet")]
         public void GetDatabaseHistoryDifferenceTest()
         {
@@ -96,9 +217,6 @@ namespace SqlSync.SqlBuild.Dependent.UnitTest
             Assert.AreEqual("7E704479328C9B2FDA48C7CF093B16F125EAB98A", actual.BuildFileHistory[2].BuildFileHash);
         }
 
-        /// <summary>
-        ///A test for GetDatabaseHistoryDifference
-        ///</summary>
         [TestMethod(), Ignore("Don't have the setup scripts ready yet")]
         public void GetDatabaseHistoryDifferenceTest_PartialSync()
         {
@@ -123,34 +241,6 @@ namespace SqlSync.SqlBuild.Dependent.UnitTest
             Assert.AreEqual("7E704479328C9B2FDA48C7CF093B16F125EAB98A", actual.BuildFileHistory[0].BuildFileHash);
         }
 
-        /// <summary>
-        ///A test for GetDatabaseHistoryDifference
-        ///</summary>
-        [TestMethod()]
-        public void GetDatabaseHistoryDifferenceTest_SameSource()
-        {
-            DatabaseDiffer target = new DatabaseDiffer();
-            ConnectionData goldenCopy = new ConnectionData()
-            {
-                DatabaseName = "SqlBuildTest_SyncTest1",
-                SQLServerName = @"localhost\SQLEXPRESS",
-                AuthenticationType = AuthenticationType.Windows
-            };
-            ConnectionData toBeUpdated = new ConnectionData()
-            {
-                DatabaseName = "SqlBuildTest_SyncTest1",
-                SQLServerName = @"localhost\SQLEXPRESS",
-                AuthenticationType = AuthenticationType.Windows
-            };
-            DatabaseRunHistory actual;
-            actual = target.GetDatabaseHistoryDifference(goldenCopy, toBeUpdated);
-            Assert.AreEqual(0, actual.BuildFileHistory.Count);
-
-        }
-
-        /// <summary>
-        ///A test for GetDatabaseRunHistory
-        ///</summary>
         [TestMethod(), Ignore("Don't have the setup scripts ready yet")]
         public void GetDatabaseRunHistoryTest_TestCountAndSelection()
         {
@@ -169,5 +259,55 @@ namespace SqlSync.SqlBuild.Dependent.UnitTest
             Assert.AreEqual(DateTime.Parse("2014-07-21 13:58:07.880"),
                 actual.BuildFileHistory.Where(x => x.BuildFileHash == "7651E282160CAF9C92CB923004D94B91181C077E").Select(y => y.CommitDate).FirstOrDefault());
         }
+
+        #endregion
+
+        #region BuildFileHistory Tests
+
+        [TestMethod]
+        public void BuildFileHistory_Properties_SetCorrectly()
+        {
+            var now = DateTime.Now;
+            var history = new BuildFileHistory
+            {
+                BuildFileHash = "ABC123",
+                BuildFileName = "Test.sbm",
+                CommitDate = now
+            };
+
+            Assert.AreEqual("ABC123", history.BuildFileHash);
+            Assert.AreEqual("Test.sbm", history.BuildFileName);
+            Assert.AreEqual(now, history.CommitDate);
+        }
+
+        #endregion
+
+        #region DatabaseRunHistory Tests
+
+        [TestMethod]
+        public void DatabaseRunHistory_Constructor_InitializesEmptyList()
+        {
+            var history = new DatabaseRunHistory();
+
+            Assert.IsNotNull(history.BuildFileHistory);
+            Assert.AreEqual(0, history.BuildFileHistory.Count);
+        }
+
+        [TestMethod]
+        public void DatabaseRunHistory_AddRange_Works()
+        {
+            var history = new DatabaseRunHistory();
+            var items = new List<BuildFileHistory>
+            {
+                new BuildFileHistory { BuildFileName = "Build1.sbm" },
+                new BuildFileHistory { BuildFileName = "Build2.sbm" }
+            };
+
+            history.BuildFileHistory.AddRange(items);
+
+            Assert.AreEqual(2, history.BuildFileHistory.Count);
+        }
+
+        #endregion
     }
 }
