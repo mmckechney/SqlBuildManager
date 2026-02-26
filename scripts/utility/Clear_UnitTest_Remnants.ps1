44124eec172df225662031793fdae0d4541d4b5c
Michael McKechney <michael@mckechney.com>
2026-02-26 14:51:55 -0500

    Enhance cleanup scripts to run in parallel for improved performance and efficiency

--- scripts/utility/Clear_UnitTest_Remnants.ps1
+++ scripts/utility/Clear_UnitTest_Remnants.ps1
@@ -10,28 +10,69 @@ if("" -eq $resourceGroupName)
     $resourceGroupName = "$prefix-rg"
 }
 
-.\Clear_UnitTest_Batch_Jobs.ps1 -prefix $prefix -includeActive $includeActiveBatchJobs
-.\Clear_UnitTest_ServiceBus_Topics.ps1 -prefix $prefix
-.\Clear_UnitTest_StorageAcct_Containers.ps1 -prefix $prefix
+# Run cleanup scripts in parallel
+$jobs = @()
+
+$jobs += Start-Job -ScriptBlock {
+    param($prefix, $includeActiveBatchJobs)
+    Set-Location $using:PWD
+    .\Clear_UnitTest_Batch_Jobs.ps1 -prefix $prefix -includeActive $includeActiveBatchJobs
+} -ArgumentList $prefix, $includeActiveBatchJobs -Name "BatchJobs"
+
+$jobs += Start-Job -ScriptBlock {
+    param($prefix)
+    Set-Location $using:PWD
+    .\Clear_UnitTest_ServiceBus_Topics.ps1 -prefix $prefix
+} -ArgumentList $prefix -Name "ServiceBus"
+
+$jobs += Start-Job -ScriptBlock {
+    param($prefix)
+    Set-Location $using:PWD
+    .\Clear_UnitTest_StorageAcct_Containers.ps1 -prefix $prefix
+} -ArgumentList $prefix -Name "Storage"
+
 if($removeAllContainerApps)
 {
-    .\Clean-ContainerApps.ps1 -resourceGroupName $resourceGroupName
+    $jobs += Start-Job -ScriptBlock {
+        param($resourceGroupName)
+        Set-Location $using:PWD
+        .\Clean-ContainerApps.ps1 -resourceGroupName $resourceGroupName
+    } -ArgumentList $resourceGroupName -Name "ContainerApps"
 }
-.\Clear_UnitTest_YamlFiles.ps1 
-.\Clear_UnitTest_AciJson.ps1 
 
-Write-Host "Deleting Kubernetes Unit Test resources in sqlbuildmanager namespace" -ForegroundColor Green
-kubectl delete all --all -n sqlbuildmanager
+$jobs += Start-Job -ScriptBlock {
+    Set-Location $using:PWD
+    .\Clear_UnitTest_YamlFiles.ps1
+} -Name "YamlFiles"
 
-Write-Host "Deleting Kubernetes Unit Test ConfigMaps" -ForegroundColor Green
-$configMaps = (kubectl get configmap -n sqlbuildmanager -o name)
-foreach($map in $configMaps) 
-{ 
-    if($map -ne "configmap/kube-root-ca.crt") 
+$jobs += Start-Job -ScriptBlock {
+    Set-Location $using:PWD
+    .\Clear_UnitTest_AciJson.ps1
+} -Name "AciJson"
+
+$jobs += Start-Job -ScriptBlock {
+    Write-Host "Deleting Kubernetes Unit Test resources in sqlbuildmanager namespace" -ForegroundColor Green
+    kubectl delete all --all -n sqlbuildmanager
+} -Name "K8sResources"
+
+$jobs += Start-Job -ScriptBlock {
+    Write-Host "Deleting Kubernetes Unit Test ConfigMaps" -ForegroundColor Green
+    $configMaps = (kubectl get configmap -n sqlbuildmanager -o name)
+    foreach($map in $configMaps) 
     { 
-        kubectl delete $map -n sqlbuildmanager
+        if($map -ne "configmap/kube-root-ca.crt") 
+        { 
+            kubectl delete $map -n sqlbuildmanager
+        }
     }
-}
+} -Name "K8sConfigMaps"
 
-Write-Host "Deleting Kubernetes Unit Test secrets" -ForegroundColor Green
-kubectl delete secret  -n sqlbuildmanager --field-selector=type=Opaque
+$jobs += Start-Job -ScriptBlock {
+    Write-Host "Deleting Kubernetes Unit Test secrets" -ForegroundColor Green
+    kubectl delete secret -n sqlbuildmanager --field-selector=type=Opaque
+} -Name "K8sSecrets"
+
+# Wait for all jobs to complete and display output
+Write-Host "Running cleanup scripts in parallel..." -ForegroundColor Cyan
+$jobs | Wait-Job | Receive-Job
+$jobs | Remove-Job
