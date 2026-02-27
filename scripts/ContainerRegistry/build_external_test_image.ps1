72565f47c174bb73d4ce9294d4277405655a0d99
Michael McKechney <michael@mckechney.com>
2026-02-27 10:16:22 -0500

    Add --no-logs option to build commands in test image scripts for cleaner output

--- scripts/ContainerRegistry/build_external_test_image.ps1
+++ scripts/ContainerRegistry/build_external_test_image.ps1
@@ -72,6 +72,7 @@ try {
         --resource-group $resourceGroupName `
         --image "${testImageName}:${imageTag}" `
         --file "$tempContext\$dockerfileName" `
+        --no-logs `
         $tempContext
     
     if ($LASTEXITCODE -ne 0) {
