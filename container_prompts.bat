50107bed2cdb51e83e914da851a94369bfd56901
Michael McKechney <michael@mckechney.com>
2026-06-05 12:10:23 -0400

    updating code paths in batch files

--- container_prompts.bat
+++ container_prompts.bat
@@ -4,7 +4,7 @@
 setlocal
 
 set "PWSH=C:\Program Files\PowerShell\7\pwsh.exe"
-set "WD=C:\Users\mimcke\source\repos\SqlBuildManager\scripts\ContainerRegistry"
+set "WD=D:\repos\SqlBuildManager\scripts\ContainerRegistry"
 
 
 wt.exe -w new ^
@@ -13,7 +13,7 @@ wt.exe -w new ^
   ; new-tab -p "PowerShell" --title "Runtime_Img"   -d "%WD%" -- "%PWSH%" -NoExit -Command "ls\; Set-PSReadLineKeyHandler -Key F12 -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::Insert('.\build_runtime_image_fromprefix.ps1 -prefix ') }\; Write-Host ''\; Write-Host 'Press F12 to load command at prompt' -ForegroundColor Cyan"
 
 
-set "WD=C:\Users\mimcke\source\repos\SqlBuildManager\scripts\tests"
+set "WD=D:\repos\SqlBuildManager\scripts\tests"
 wt.exe -w new ^
   new-tab -p "PowerShell" --title "External_Img"  -d "%WD%" -- "%PWSH%" -NoExit  -Command "ls\; Set-PSReadLineKeyHandler -Key F12 -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::Insert('.\run_all_external_tests_in_aci.ps1 -prefix ') }\; Write-Host ''\; Write-Host 'Press F12 to load command at prompt' -ForegroundColor Cyan" ^
   ; new-tab -p "PowerShell" --title "Dependent_Img" -d "%WD%" -- "%PWSH%" -NoExit -Command "ls\; Set-PSReadLineKeyHandler -Key F12 -ScriptBlock { [Microsoft.PowerShell.PSConsoleReadLine]::Insert('.\run_dependent_tests_in_aci.ps1 -prefix ') }\; Write-Host ''\; Write-Host 'Press F12 to load command at prompt' -ForegroundColor Cyan"
